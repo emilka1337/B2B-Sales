95a8effabbc54f1ddc9502d3ee14ef11085df483
Emil <maxmudov2000@gmail.com>
2022-04-14 21:53:11 +0400

    cart added

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+let flkty = new Flickity('.main-carousel', {
+    cellAlign: 'center',
+    wrapAround: true,
+    autoPlay: true,
+    pageDots: false,
+    // arrowShape: "x0: 18.88, x1: 20.8, y1: 33.25, x2: 34.41"
+});
